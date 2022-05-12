4b8a78472d40c461a7ec7331b929beaab075ddf6
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2022-05-12 16:38:47 -0300

    herança

---
+++ Herança/Aluno.java
@@ -0,0 +1,5 @@
+package Herança;
+
+public class Aluno {
+
+}
---
+++ Herança/Gen.java
@@ -0,0 +1,10 @@
+package Herança;
+
+public class Gen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
